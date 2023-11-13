b229696d52a6e87fce263ea44ba92e14277eb189
Usama Farooq <148746261+Usama299792458@users.noreply.github.com>
2023-11-13 12:35:43 +0500

    Add files via upload

---
+++ Array.m
@@ -0,0 +1,20 @@
+Array1=[1 2 3 4 5 6]
+Array2=[1 2 3; 4 5 6; 7 8 9]
+Array3=[1; 2; 3; 4]
+v=[1 3,sqrt(5)]
+v2=[3 + 4 5]
+v3=[3 4 5]
+Array3=0:5
+start=0;
+step=0.25;
+stop=4;
+Array4=start:step:stop
+sum4=sum(Array4)
+sin (pi/4)
+sin([0*pi/4*pi/23*pi/4*pi])
+u=zeros(1, 10)
+u(5)=1
+stem(u)
+t=-1:.0005:1;
+xt=sin(2* pi* 5.*t +pi/6); 
+plot(t,xt,'b-')
